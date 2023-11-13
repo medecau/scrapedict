3d7d6f8abbe1db05f0e8c01037c5a51638fc31b2
Pedro Rodrigues <me@pdbr.org>
2023-11-13 08:19:21 +0100

    Refactor tests to use a fixture for the soup

--- test_scrapedict.py
+++ test_scrapedict.py
@@ -1,3 +1,4 @@
+import pytest
 from bs4 import BeautifulSoup
 
 import scrapedict as sd
@@ -18,39 +19,42 @@ sample = """<html>
 </html>"""
 
 
-def test_cook_returns_soup():
-    soup = sd.cook(sample)
+@pytest.fixture
+def soup():
+    return sd.cook(sample)
+
+
+def test_cook_returns_soup(soup):
+    # assumes that the soup fixture uses the cook function
 
     assert isinstance(soup, BeautifulSoup)
 
 
-def test_html():
-    soup = sd.cook(sample)
+def test_cook_ignores_soup(soup):
+    new_soup = sd.cook(soup)
 
+    assert new_soup is soup
+
+
+def test_html(soup):
     html_header_selector = sd.html("header")
 
     assert html_header_selector(soup) == soup.header
 
 
-def test_attr():
-    soup = sd.cook(sample)
-
+def test_attr(soup):
     attr_href_selector = sd.attr("a", "href")
 
     assert attr_href_selector(soup) == soup.a["href"]
 
 
-def test_text():
-    soup = sd.cook(sample)
-
+def test_text(soup):
     text_h1_selector = sd.text("h1")
 
     assert text_h1_selector(soup) == soup.h1.string
 
 
-def test_parse():
-    soup = sd.cook(sample)
-
+def test_match(soup):
     parse_article_selector = sd.parse("article", "Paragraph of the {}.")
 
     assert parse_article_selector(soup)[0] == "article"
